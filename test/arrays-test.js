d391f2402d933b85b0c1034897c32aa2e56f58eb
Rose Stough <psithurise@programmer.net>
2019-03-19 21:03:29 +0000

    Automatically backed up by Learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -10,11 +10,7 @@ describe('arrays', () => {
     src: fs.readFileSync(path.resolve(__dirname, '..', 'arrays.js'), 'utf-8')
   })
 
-  describe('chocolateBars', () => {
-    it('is an array containing "snickers", "hundred grand", "kitkat", and "skittles"', () => {
-      expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles']
-    })
-  })
+
 
   describe('addElementToBeginningOfArray(array, element)', () => {
     it('adds an element to the beginning of an array', () => {
